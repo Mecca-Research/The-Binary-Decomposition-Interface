02b2a5a27fe86d08391bf069eec0ec92f585508f
Tariq Mohammed <thehealthfreaktv@gmail.com>
2025-08-08 17:25:28 -0400

    Create types.hpp

---
+++ BDI_cpp/include/bdi/types.hpp
@@ -0,0 +1,6 @@
+// Auto-generated umbrella header for public BDI API (initial draft)
+#pragma once
+#include "BDI_cpp/Core/Types/BDIValueVariant.hpp"
+#include "BDI_cpp/Core/Types/BinaryEncoding.hpp"
+#include "BDI_cpp/Core/Types/TypeSystem.hpp"
+#include "BDI_cpp/Core/Types/VMTypeOperations.hpp"
