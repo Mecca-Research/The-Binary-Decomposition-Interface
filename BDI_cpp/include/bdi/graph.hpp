6a1756e8c8c3f16f6c1734cdddd7c28d560e04d1
Tariq Mohammed <thehealthfreaktv@gmail.com>
2025-08-08 17:23:37 -0400

    Create graph.hpp

---
+++ BDI_cpp/include/bdi/graph.hpp
@@ -0,0 +1,7 @@
+// Auto-generated umbrella header for public BDI API (initial draft)
+#pragma once
+#include "BDI_cpp/core/graph/BDIGraph.hpp"
+#include "BDI_cpp/core/graph/BDINode.hpp"
+#include "BDI_cpp/core/graph/GraphVisitor.hpp"
+#include "BDI_cpp/core/graph/GraphVisualizer.hpp"
+#include "BDI_cpp/core/graph/OperationTypes.hpp"
