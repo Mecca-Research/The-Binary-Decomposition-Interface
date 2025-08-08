641f5b755d1de09aff2804afa640d1faa91f3fa3
Tariq Mohammed <thehealthfreaktv@gmail.com>
2025-08-08 17:24:48 -0400

    runtime.hpp

---
+++ BDI_cpp/include/bdi/runtime.hpp
@@ -0,0 +1,7 @@
+// Auto-generated umbrella header for public BDI API (initial draft)
+#pragma once
+#include "BDI_cpp/Runtime/VM/BDIVirtualMachine.hpp"
+#include "BDI_cpp/Runtime/VM/ExecutionContext.hpp"
+#include "BDI_cpp/Runtime/Memory/MemoryAllocator.hpp"
+#include "BDI_cpp/Runtime/Memory/MemoryManager.hpp"
+#include "BDI_cpp/Runtime/Memory/MemoryRegion.hpp"
