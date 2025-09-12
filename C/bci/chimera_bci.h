7d9462b9d7d4a1e0e689665e05c5ed6c8e98e272
Tariq Mohammed <thehealthfreaktv@gmail.com>
2025-09-12 18:42:40 -0400

    Update chimera_bci.h

--- C/bci/chimera_bci.h
+++ C/bci/chimera_bci.h
@@ -16,18 +16,3 @@ void print_binary_float(float f);
 
 
 #endif // CHIMERA_BCI_H
-
-
-// ----------- HEADER: chimera_btl.h -------------
-#ifndef CHIMERA_BTL_H
-#define CHIMERA_BTL_H
-
-
-#include <stdint.h>
-
-
-const char* get_isa_mnemonic(uint8_t opcode);
-const char* decode_flag_state(uint8_t flags);
-
-
-#endif // CHIMERA_BTL_H
